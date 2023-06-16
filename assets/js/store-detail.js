520b6a8ebca496f26cc56da074f7e41f809d8ff7
Kyoung <105770887+taeseokyoung@users.noreply.github.com>
2023-06-16 16:44:50 +0900

    [병합] 충돌 병합

--- assets/js/store-detail.js
+++ assets/js/store-detail.js
@@ -13,7 +13,7 @@ window.onload = async function HanbokStoreDetail() {
     // hanbokstore_id = urlParams.get('hanbokstore_id');
     hanbokstore_id = 1
 
-    const response = await fetch(`${backend_base_url}/api/v1/stores/${hanbokstore_id}`,{
+    const response = await fetch(`${backend_base_url}/api/v1/stores/${hanbokstore_id}`, {
     })
     if (response.status == 200) {
         const response_json = await response.json()
@@ -24,12 +24,12 @@ window.onload = async function HanbokStoreDetail() {
         const get_x = response_json.Store.location_x
         const get_y = response_json.Store.location_y
         const get_star = response_json.Store.star
-        
+
         const store_name = document.getElementById('store_name');
         const store_address = document.getElementById('store_address');
         store_name.innerText = get_name
         store_address.innerText = get_address
-        
+
         const hanbok = document.getElementById('hanbok_list');
         console.log(response_json.HanbokList)
         response_json.HanbokList.forEach(hanboks => {
@@ -51,10 +51,10 @@ window.onload = async function HanbokStoreDetail() {
             const price = document.createElement("p");
             price.setAttribute("class", "event-price")
             price.innerText = hanboks.hanbok_price + " ~"
-            
+
             const btn = document.createElement("button");
             btn.setAttribute("type", "button")
-            btn.setAttribute("onclick",`SelectItem(${hanboks.id})`)
+            // btn.setAttribute("onclick",`SelectItem(${hanboks.id})`)
             btn.innerText = "예약하기"
 
             div.appendChild(img)
@@ -63,7 +63,7 @@ window.onload = async function HanbokStoreDetail() {
             div2.appendChild(price)
             div2.appendChild(btn)
 
-            hanbok.insertBefore(div,hanbok.firstChild);
+            hanbok.insertBefore(div, hanbok.firstChild);
         })
 
     } else {
@@ -72,5 +72,5 @@ window.onload = async function HanbokStoreDetail() {
 }
 
 async function SelectItem(hanbok_id) {
-    window.location.href = `${frontend_base_url}/select.html?hanbok_id=${hanbok_id}`
+    window.location.href = `${frontend_base_url}/doc/select.html?hanbok_id=${hanbok_id}`
 }
