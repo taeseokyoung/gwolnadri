2fc76b04b49d7def2faa164fe047fb4d5fb30886
BenCyera <dltpgml1124@gmail.com>
2023-06-16 16:25:47 +0900

    [수정] 로케이션 주소 수정 #8
    assets/store-detail.js : doc 삭제 , onClick 주석 해제

--- assets/js/store-detail.js
+++ assets/js/store-detail.js
@@ -54,7 +54,7 @@ window.onload = async function HanbokStoreDetail() {
             
             const btn = document.createElement("button");
             btn.setAttribute("type", "button")
-            // btn.setAttribute("onclick",`SelectItem(${hanboks.id})`)
+            btn.setAttribute("onclick",`SelectItem(${hanboks.id})`)
             btn.innerText = "예약하기"
 
             div.appendChild(img)
@@ -64,7 +64,7 @@ window.onload = async function HanbokStoreDetail() {
             div2.appendChild(btn)
 
             hanbok.insertBefore(div,hanbok.firstChild);
-    })
+        })
 
     } else {
         alert(response.status)
@@ -72,5 +72,5 @@ window.onload = async function HanbokStoreDetail() {
 }
 
 async function SelectItem(hanbok_id) {
-    window.location.href = `${frontend_base_url}/doc/select.html?hanbok_id=${hanbok_id}`
+    window.location.href = `${frontend_base_url}/select_hanbok.html?hanbok_id=${hanbok_id}`
 }
