ec8bb22770e07adf2945f326d3416109499cc324
Ra0ul <nasj1621@gmail.com>
2023-06-18 21:33:27 +0900

    [추가] 한복점 지도 추가 #6
    23.06.18
    
    assets/js/store.js : 지도 api 추가
    store.html : 지도 위 한복점 마커 생성

--- assets/js/store.js
+++ assets/js/store.js
@@ -1,13 +1,12 @@
 
 window.onload = async function loadStoreList() {
-    stores = await store()
-    console.log("여기여기여기 : ", stores["StoreList"][0])
-
+    
+    var positions =[]
+    //한복점 html 카드 생성
     const storeCard = document.getElementById("store-list-body")
-
+    stores = await store()
+    // console.log("여기여기여기 : ", stores["StoreList"][0])
     stores["StoreList"].forEach(store => {
-        console.log(store.store_name)
-
         
         const newCon = document.createElement("div")
         const newTitle = document.createElement("div")
@@ -35,7 +34,6 @@ window.onload = async function loadStoreList() {
         newHeart.appendChild(newHeartImg)
         newHeart.appendChild(newHeartNum)
         newBook.appendChild(newBookImg)
-        
 
         newCon.setAttribute("class", "contant-card")
         newTitle.setAttribute("class", "store-title")
@@ -49,14 +47,50 @@ window.onload = async function loadStoreList() {
         newHeartImg.setAttribute("alt", "")
         newBookImg.setAttribute("src", "../assets/img/Bookmark-outline.svg")
         newBookImg.setAttribute("alt", "")
+        //북마크 표시
+        // if (store.store_bookmarks == 1){
+        //     newBookImg.setAttribute("src", "../assets/img/Bookmark-full.svg")
+        // } else {
+        //     newBookImg.setAttribute("src", "../assets/img/Bookmark-outline.svg")
+        // }
 
         newStore.innerText=store.store_name
         newAdd.innerText=store.store_address
         newHeartNum.innerText=store.total_likes
         newCate.innerText = "전통한복"  //카테고리 가져오기
         
-    });
-    
+        
+        //한복점 포지션 리스트 생성
+        positions.push({
+            title: store.store_name,
+            latlng: new kakao.maps.LatLng(store.location_y,store.location_x)   
+        })
+        
+    })
+
+    //지도 생성
+    var mapContainer = document.getElementById('map')
+    var mapOptions = {
+        center: new kakao.maps.LatLng( 37.5714476873524,126.998320034926),
+        level: 3
+    }
+    var map = new kakao.maps.Map(mapContainer, mapOptions);
+ 
+
+    // 마커 이미지 생성
+    var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+        
+    for (var i = 0; i < positions.length; i ++) {
+        var imageSize = new kakao.maps.Size(24, 35);  
+        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+        var marker = new kakao.maps.Marker({
+            map: map,
+            position: positions[i].latlng,
+            title : positions[i].title,
+            image : markerImage 
+        })
+    }
+   
 }
 
 //한복집 리스트 가져오기
