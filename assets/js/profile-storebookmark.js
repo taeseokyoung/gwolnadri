49f7d1f8f924a9b99c8143497dc7ffa6e86237be
BenCyera <dltpgml1124@gmail.com>
2023-06-25 15:15:11 +0900

    [추가] 관심 한복점 예외사항처리 #5
    profile-bookmark.css : icon css 추가
    profile-storeBookmark.html : 예외사항 html추가
    profile-storebookmark.js : if문 추가

--- assets/js/profile-storebookmark.js
+++ assets/js/profile-storebookmark.js
@@ -2,56 +2,75 @@
 window.onload = async function loadStoreList() {
     
     const payload = localStorage.getItem("payload")
-    
 
     //한복점 html 카드 생성
     const storeCard = document.getElementById("store-list-body")
     bookedStores = await bookedStore()
     console.log(bookedStores["bookmark_stores"])
-    bookedStores["bookmark_stores"].forEach(store => {
-        console.log(store.id)
-        const newCon = document.createElement("div") 
-        const newTitle = document.createElement("div")
-        const newCate = document.createElement("p")
-        const newStore = document.createElement("p")
-        const newAdd = document.createElement("p")
-        const newIcon = document.createElement("div")
-        const newHeart = document.createElement("div")
-        const newBook = document.createElement("div")
-        const newHeartImg = document.createElement("img")
-        const newBookImg = document.createElement("img")
-        const newHeartNum = document.createElement("span")
-        let likeOn
-        let bookOn
-        
-        storeCard.appendChild(newCon)
-        
-        newCon.appendChild(newTitle)
-        newTitle.appendChild(newCate)
-        newTitle.appendChild(newStore)
-        newTitle.appendChild(newAdd)
 
-        newCon.append(newIcon)
-        newIcon.appendChild(newHeart)
-        newIcon.appendChild(newBook)
-        newHeart.appendChild(newHeartImg)
-        newHeart.appendChild(newHeartNum)
-        newBook.appendChild(newBookImg)
+    console.log(bookedStores["bookmark_stores"].length)
+    if (bookedStores["bookmark_stores"].length == 0) {
+        const none_list = document.createElement('div')
+        none_list.setAttribute('class','sh_col')
 
+        const i = document.createElement('i')
+        i.setAttribute('class','xi-shop')
+
+        const none_content = document.createElement('p')
+        none_content.setAttribute('class','sh_small')
+        none_content.innerText = "관심 한복점이 없습니다"
+
+        none_list.appendChild(i)
+        none_list.appendChild(none_content)
+        storeCard.insertBefore(none_list, storeCard.firstChild);
+    } else {
+
+        bookedStores["bookmark_stores"].forEach(store => {
         
-        newCon.setAttribute("class", "contant-card")
-        newTitle.setAttribute("class", "store-title")
-    
-        newCate.setAttribute("class", "hanbok_category")
-        newStore.setAttribute("class", "hanbok_store")
-        newTitle.setAttribute("onclick", "storeLink("+store.id+")")
-        newAdd.setAttribute("class", "hanbok_address")
-        newIcon.setAttribute("class", "card-icon")
-        newHeart.setAttribute("class", "heart")
-        newBook.setAttribute("class", "bookmark")
-        newHeartImg.setAttribute("src", "/assets/img/Heart-outline.svg")
-        newHeartImg.setAttribute("alt", "")
-        newBookImg.setAttribute("alt", "")
+            console.log(store.id)
+            const newCon = document.createElement("div") 
+            const newTitle = document.createElement("div")
+            const newCate = document.createElement("p")
+            const newStore = document.createElement("p")
+            const newAdd = document.createElement("p")
+            const newIcon = document.createElement("div")
+            const newHeart = document.createElement("div")
+            const newBook = document.createElement("div")
+            const newHeartImg = document.createElement("img")
+            const newBookImg = document.createElement("img")
+            const newHeartNum = document.createElement("span")
+            let likeOn
+            let bookOn
+            
+            storeCard.appendChild(newCon)
+            
+            newCon.appendChild(newTitle)
+            newTitle.appendChild(newCate)
+            newTitle.appendChild(newStore)
+            newTitle.appendChild(newAdd)
+
+            newCon.append(newIcon)
+            newIcon.appendChild(newHeart)
+            newIcon.appendChild(newBook)
+            newHeart.appendChild(newHeartImg)
+            newHeart.appendChild(newHeartNum)
+            newBook.appendChild(newBookImg)
+
+            
+            newCon.setAttribute("class", "contant-card")
+            newTitle.setAttribute("class", "store-title")
+        
+            newCate.setAttribute("class", "hanbok_category")
+            newStore.setAttribute("class", "hanbok_store")
+            newTitle.setAttribute("onclick", "storeLink("+store.id+")")
+            newAdd.setAttribute("class", "hanbok_address")
+            newIcon.setAttribute("class", "card-icon")
+            newHeart.setAttribute("class", "heart")
+            newBook.setAttribute("class", "bookmark")
+            newHeartImg.setAttribute("src", "/assets/img/Heart-outline.svg")
+            newHeartImg.setAttribute("alt", "")
+            newBookImg.setAttribute("alt", "")
+        
 
         //로그인 여부 판단
         if (payload){
@@ -85,9 +104,10 @@ window.onload = async function loadStoreList() {
         newAdd.innerText=store.store_address
         newHeartNum.innerText=store.total_likes
         newCate.innerText = "전통한복"  //카테고리 가져오기
-        
+    
+       
     })
-      
+}  
 }
 
 //✅ 북마크한 한복집 리스트 가져오기
