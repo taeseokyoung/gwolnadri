178a0566bd1f363363ca032c198935b6551d77a2
Jeongchan-ho <zmxndk1@gmail.com>
2023-06-26 16:57:04 +0900

    [수정] 찾기 펭이지에서 좋아요 기능 구현 #77
    js/search.js: 좋아요 아이콘 구분, 좋아요 기능 구현

--- assets/js/search.js
+++ assets/js/search.js
@@ -76,29 +76,36 @@ window.onload = async function SelectTicket(search) {
             event_date.innerText = `${events.event_start_date}`+ " - "+`${events.event_end_date}`
 
             // 좋아요, 북마크 아이콘
-            // const icon_div = document.createElement('div')
-            // icon_div.setAttribute('class','card-icon')
+            const icon_div = document.createElement('div')
+            icon_div.setAttribute('class','card-icon')
 
-            // const like_div = document.createElement('div')
-            // like_div.setAttribute('class','heart')
+            const like_div = document.createElement('div')
+            like_div.setAttribute('class','heart')
 
-            // const like_icon = document.createElement('img')
+            const like_icon = document.createElement('img')
+            if (!payload_parse || !payload_parse.user_id) { 
+              like_icon.setAttribute("src", "/assets/img/Heart-outline.svg");
+            } else if (events.likes.includes(payload_parse.user_id)) {
+              like_icon.setAttribute("src", "/assets/img/Heart-full.svg");
+            } else{
+              like_icon.setAttribute("src", "/assets/img/Heart-outline.svg");
+            }
             // like_icon.setAttribute('src',"/assets/img/Heart-outline.svg")
 
-            // const like_span = document.createElement('span')
-            // like_span.innerText = `${events.likes_count}`
+            const like_span = document.createElement('span')
+            like_span.innerText = `${events.likes_count}`
 
-            // const bookmark_div = document.createElement('div')
-            // bookmark_div.setAttribute('class','bookmark') 
+            const bookmark_div = document.createElement('div')
+            bookmark_div.setAttribute('class','bookmark') 
 
-            // const bookmark_icon = document.createElement('img')
-            // if (!payload_parse || !payload_parse.user_id) {
-            //     bookmark_icon.setAttribute("src", "/assets/img/Bookmark-outline.svg");
-            // } else if (events.event_bookmarks.includes(payload_parse.user_id)) {
-            //     bookmark_icon.setAttribute("src", "/assets/img/Bookmark-full.svg");
-            // } else {
-            //     bookmark_icon.setAttribute("src", "/assets/img/Bookmark-outline.svg");
-            // }
+            const bookmark_icon = document.createElement('img')
+            if (!payload_parse || !payload_parse.user_id) {
+                bookmark_icon.setAttribute("src", "/assets/img/Bookmark-outline.svg");
+            } else if (events.event_bookmarks.includes(payload_parse.user_id)) {
+                bookmark_icon.setAttribute("src", "/assets/img/Bookmark-full.svg");
+            } else {
+                bookmark_icon.setAttribute("src", "/assets/img/Bookmark-outline.svg");
+            }
 
             div.appendChild(event_img)
             div.appendChild(season)
@@ -106,44 +113,70 @@ window.onload = async function SelectTicket(search) {
             txt_div.appendChild(category)
             txt_div.appendChild(event_title)
             txt_div.appendChild(event_date)
-            // txt_div.appendChild(icon_div)
-            // icon_div.appendChild(like_div)
-            // icon_div.appendChild(bookmark_div)
-            // like_div.appendChild(like_icon)
-            // like_div.appendChild(like_span)
-            // bookmark_div.appendChild(bookmark_icon)
+            txt_div.appendChild(icon_div)
+            icon_div.appendChild(like_div)
+            icon_div.appendChild(bookmark_div)
+            like_div.appendChild(like_icon)
+            like_div.appendChild(like_span)
+            bookmark_div.appendChild(bookmark_icon)
+            event_list.insertBefore(div, event_list.firstChild);
 
             event_img.addEventListener('click', function () {
                 const event_id = parseInt(events.id, 10);
                 window.location.href = `${frontend_base_url}/event-detail.html?event_id=${event_id}`;
             });
 
-            // bookmark_icon.addEventListener('click', async () => {
-            //     const event_id = parseInt(events.id, 10);
+            bookmark_icon.addEventListener('click', async () => {
+                const event_id = parseInt(events.id, 10);
           
-            //     // console.log(event_id);
-            //     if (payload) {
-            //       try {
-            //         const bookmarkResponse = await fetch(`${backend_base_url}/events/${event_id}/bookmark/`, {
-            //           method: 'POST',
-            //           headers: {
-            //             "Authorization": `Bearer ${token}`
-            //           }
-            //         });
+                // console.log(event_id);
+                if (payload) {
+                  try {
+                    const bookmarkResponse = await fetch(`${backend_base_url}/events/${event_id}/bookmark/`, {
+                      method: 'POST',
+                      headers: {
+                        "Authorization": `Bearer ${token}`
+                      }
+                    });
           
-            //         const bookmarkData = await bookmarkResponse.json();
-            //         alert(bookmarkData.message);
-            //       } catch (error) {
-            //         console.error('Error bookmarking event:', error);
-            //       }
-            //     } else {
-            //       alert("로그인이 필요합니다")
-            //     }
-            //     window.location.reload()
+                    const bookmarkData = await bookmarkResponse.json();
+                    alert(bookmarkData.message);
+                  } catch (error) {
+                    console.error('Error bookmarking event:', error);
+                  }
+                } else {
+                  alert("로그인이 필요합니다")
+                }
+                window.location.reload()
           
-            // });
+            });
             
-            event_list.insertBefore(div, event_list.firstChild);
+            like_icon.addEventListener('click', async () => {
+              const event_id = parseInt(events.id, 10);
+              const token = localStorage.getItem("access");
+              if (payload) {
+                try {
+                  const heartResponse = await fetch(`${backend_base_url}/events/${event_id}/like/`, {
+                    method: 'POST',
+                    headers: {
+                      "Authorization": `Bearer ${token}`
+                    }
+                  });
+          
+                  const heartData = await heartResponse.json();
+                  alert(heartData.message);
+          
+                } catch (error) {
+                  console.error('Error likes event:', error);
+                }
+              }
+              else {
+                alert("로그인이 필요합니다")
+              }
+          
+              window.location.reload()
+          
+            });            
         }
         
     }
