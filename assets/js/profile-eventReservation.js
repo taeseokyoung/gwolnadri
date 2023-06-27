f5745cca8704ab0839bb4116af224431b16c6c37
BenCyera <dltpgml1124@gmail.com>
2023-06-28 02:41:19 +0900

    [수정] 예매, 예약내역 수정 #31
    profile-eventReservation.html : 데이터 없을 시 예외사항 추가
    profile-storeReservation.html : 데이터 없을 시 예외사항 추가
    reservation_detail.html : 텍스트 수정
    profile-eventReservation.js : 예외사항 스크립트 추가
    profile-storeReservation.js : 위와 같음
    profile-storebookmark.js : const 추가

--- assets/js/profile-eventReservation.js
+++ assets/js/profile-eventReservation.js
@@ -1,4 +1,7 @@
 window.onload = async function EventReservation() {
+
+    const card_list = document.getElementById('card_list');
+
     // event 예약내역 조회
     const response = await fetch(`${backend_base_url}/api/v1/stores/payment/${payload_parse.user_id}/event/`, {
         headers: {
@@ -7,17 +10,36 @@ window.onload = async function EventReservation() {
     })
     if (response.status == 200) {
         const response_json = await response.json()
-        // console.log(response_json)
 
-        // tid 출력
-        for (let i = 0; i < response_json.length; i++) {
+        if (response_json.length == 0) {
+
+            // 예약이 없습니다
+            const none_list = document.createElement('div')
+            none_list.setAttribute('class','sh_col')
+
+            const i = document.createElement('i')
+            i.setAttribute('class','xi-calendar-cancle')
+
+            const none_content = document.createElement('p')
+            none_content.setAttribute('class','sh_small')
+            none_content.innerText = "예매한 행사가 없습니다"
+
+            none_list.appendChild(i)
+            none_list.appendChild(none_content)
+            card_list.insertBefore(none_list, card_list.firstChild);
+        } else {
+            // tid 출력
+            for (let i = 0; i < response_json.length; i++) {
+
             const tid = response_json[i].tid
             const order_id = response_json[i].partner_order_id
             const item_name = response_json[i].item_name
             const quantity = response_json[i].quantity
             
-            const ticket_id = String(order_id).split('2023')[1]            
-             
+            const ticket_id = String(order_id).split('2023')[1]
+            console.log(order_id)
+            console.log(ticket_id)
+            
             // ticket 조회
             const eventDetail = await fetch(`${backend_base_url}/events/${ticket_id}/ticket/`, {
                 headers: {
@@ -43,8 +65,6 @@ window.onload = async function EventReservation() {
 
                     const event_json = await event.json()
                     const event_img = event_json.image
-
-                    const card_list = document.getElementById('card_list');
                     
                     const subCard = document.createElement("div")
                     subCard.setAttribute("class","sub-card")
@@ -94,23 +114,18 @@ window.onload = async function EventReservation() {
                                     
                 } else {
                     alert(event.status)
-                    window.location.href = `${index_url}`
-
+                    // window.location.href = `${index_url}`
                 }
 
             } else {
                 alert(eventDetail.status)
-                window.location.href = `${index_url}`
-
-            }
-            
+                // window.location.href = `${index_url}`
+            }}
         }
-            
     } else {
         alert(response.status)
-        window.location.href = `${index_url}`
+        // window.location.href = `${index_url}`
     }
-
 }
 
 async function ReservationDetail(tid) {
